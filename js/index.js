cc243502b337f97bd3a0aa7c41f0782f976ab5e0
Zeph180 <zephrichards1@gmail.com>
2023-01-17 00:25:24 +0300

    My trials

--- js/index.js
+++ js/index.js
@@ -1,54 +1,55 @@
 const bookSection = document.querySelector('.books');
-const addBookBtn = document.querySelector('.add-btn');
 const form = document.querySelector('form');
-const books = [];
-const book = { title: '', author: '', id: '' };
 
-function RemoveBook() {
-  console.log('Btn removed');
-  console.log(books, "books");
+const books = [];
+
+function renderBook(book) {
+  const bookCont = document.createElement('article');
+  bookCont.innerHTML = `
+    <p class="book-title">${book.title}</p>
+    <p class="author">${book.author}</p>
+    <button id=${book.id} class="remove-btn">Remove</button>
+    <hr>
+  `;
+  bookSection.prepend(bookCont);
 }
 
-function AddBook() {
-  const titleInput = document.querySelector('.title').value;
-  const authorInput = document.querySelector('.author-input').value;
-  const remBtns = document.querySelectorAll('.remove-btn');
-  const bookCont = document.createElement('article');
-
-  const btns = Array.from(remBtns);
-  console.log(btns);
-
-  btns.forEach((btn) => {
-    btn.addEventListener('click', RemoveBook);
-  });
+function addBook(title, author) {
+  const book = {
+    title,
+    author,
+    id: crypto.randomUUID(),
+  };
 
   books.push(book);
-  book.title = titleInput;
-  book.author = authorInput;
-
-  books.forEach((book) => {
-    bookCont.classList.add('book');
-    bookCont.innerHTML = `
-      <p class="book-title">${book.title}</p>
-      <p class="author">${book.author}</p>
-      <button id=${book.id} class="remove-btn" onclick="RemoveBook()">Remove</button>
-      <hr>
-    `;
-    bookSection.append(bookCont);
-  });
-  form.reset();
+  renderBook(book);
 }
 
-addBookBtn.addEventListener('click', AddBook);
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const titleInput = document.querySelector('.title').value;
+  const authorInput = document.querySelector('.author-input').value;
 
-// const removeButtons = Array.from(buttons);
-// console.log(removeButtons, 'yyy');
+  if (titleInput !== '' && authorInput !== '') {
+    addBook(titleInput, authorInput);
+    form.reset();
+  }
+});
 
-// removeButtons.forEach((button, i) => {
-//   function RemoveBook() {
-//     console.log('Btn removed');
-//     removeButtons.filter((removeButtons) => !removeButtons[i]);
-//   }
+let booksData = JSON.parse(localStorage.getItem('books'));
+if (booksData !== null) {
+  booksData.forEach((book) => renderBook(book));
+} else {
+  localStorage.setItem('books', JSON.stringify(books));
+  books.forEach((book) => renderBook(book));
+}
 
-//   button.addEventListener('click', RemoveBook);
-// });
+const remove = document.querySelectorAll('.remove-btn');
+remove.forEach((item, index) => {
+  item.addEventListener('click', () => {
+    booksData = JSON.parse(localStorage.getItem('books'));
+    booksData.splice(index, 1);
+    localStorage.setItem('books', JSON.stringify(booksData));
+    window.location.reload();
+  });
+});
